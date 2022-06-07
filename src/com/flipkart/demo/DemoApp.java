5b2ecad07e79d11aed7f86a4bf49c6ee7ce42313
Hritika N Naik <hritika.naik@BLRETV-R2EAG8WN.local>
2022-06-07 08:41:05 +0530

    First commit

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author hritika.naik
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
